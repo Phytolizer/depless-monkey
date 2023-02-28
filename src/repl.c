a1a2661661dd6aa87fa66b52c808940655e1f416
Kyle Coffey <me@phytolizer.dev>
2023-02-27 19:36:10 -0600

    parse for REPL

--- src/repl.c
+++ src/repl.c
@@ -1,6 +1,7 @@
 #include "monkey/repl.h"
 
 #include "monkey/lexer.h"
+#include "monkey/parser.h"
 #include "monkey/string.h"
 
 const struct string PROMPT = STRING_REF_C(">> ");
@@ -18,16 +19,24 @@ extern void repl_start(FILE* in, FILE* out) {
 
         struct lexer lexer;
         lexer_init(&lexer, line);
-        for (struct token tok = lexer_next_token(&lexer); tok.type != TOKEN_EOF;
-             tok = lexer_next_token(&lexer)) {
-            fprintf(
-                out,
-                "{Type:" STRING_FMT " Literal:" STRING_FMT "}\n",
-                STRING_ARG(token_type_string(tok.type)),
-                STRING_ARG(tok.literal)
-            );
-            STRING_FREE(tok.literal);
+        struct parser parser;
+        parser_init(&parser, &lexer);
+        struct ast_program* program = parse_program(&parser);
+        if (parser.errors.len > 0) {
+            fprintf(out, "parser errors:\n");
+            for (size_t i = 0; i < parser.errors.len; i++) {
+                fprintf(out, "\t" STRING_FMT "\n", STRING_ARG(parser.errors.ptr[i]));
+            }
+            ast_node_free(&program->node);
+            parser_deinit(&parser);
+            continue;
         }
+
+        struct string output = ast_node_string(&program->node);
+        fprintf(out, STRING_FMT "\n", STRING_ARG(output));
+        STRING_FREE(output);
+        ast_node_free(&program->node);
+        parser_deinit(&parser);
     }
 
     STRING_FREE(line);
