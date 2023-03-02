f6d1e76cc83d6fad5da805fcd1383e3eae001e89
Kyle Coffey <me@phytolizer.dev>
2023-03-02 09:42:42 -0600

    the FIXME is fixed.

--- test/evaluator.c
+++ test/evaluator.c
@@ -440,12 +440,8 @@ SUITE_FUNC(state, evaluator) {
         "  fn(y) { x + y };\n"
         "};\n"
         "\n"
-        // this call to newAdder returns a function
-        // but its parent environment gets freed after the call
-        // solution: refcounting?
         "let addTwo = newAdder(2);\n"
         "addTwo(2);\n"
     );
-    // [FIXME] CRASH on this test (uaf in extending function environment)
     RUN_TEST(state, integer_expression, S("closure"), closure_input, 4);
 }
